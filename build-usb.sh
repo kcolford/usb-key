5eb9057a380eda409b88bd3745bf5ff3a80feba4
Kieran Colford <kieran@kcolford.com>
2019-07-25 15:13:32 -0400

    Next time someone asks you how to fix an infinite loop, remember this commit

--- build-usb.sh
+++ build-usb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-sgdisk -o "$1"			 # clear
+sgdisk -z -o "$1"		 # clear
 sgdisk -n 0:0:+1M -t 0:ef02 "$1" # grub boot
 sgdisk -n 0:0:0 -t 0:ef00 "$1"	 # data and efi
 sgdisk -h 2 "$1"		 # hybrid main
