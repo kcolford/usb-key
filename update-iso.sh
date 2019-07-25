fbc135a1e6ad8e0fc1337aa96dd4c25b7a09b8c1
Kieran Colford <kieran@kcolford.com>
2019-07-25 14:29:25 -0400

    update-iso: added label renaming

--- update-iso.sh
+++ update-iso.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-bsdtar -x -f "$1" -C "$(dirname "$0")"
+# look at device
+d="$(dirname "$0")"
+read -r fsdev < <(df -P "$d" | awk 'END{print $1}')
+read -r fsuuid < <(lsblk "$fsdev" -no uuid)
+read -r bootdev < <(lsblk "$fsdev" -no pkname)
+
+bsdtar -x -f "$1" -C "$d"
+sed -i "s|archisolabel=\w*|archisodevice=/dev/disk/by-uuid/$fsuuid|g" "$d"/{arch,loader}/**
 
 # bios compatibility
-extlinux -i "$(dirname "$0")"/arch/boot/syslinux
-read -r fsdev < <(df -P "$0" | awk 'END{print $1}')
-read -r bootdev < <(lsblk "$fsdev" -no pkname)
+extlinux -i "$d"/arch/boot/syslinux
 dd bs=440 count=1 conv=notrunc if=/usr/lib/syslinux/bios/mbr.bin of=/dev/"$bootdev"
