e8df9a750b6971df5c61b8d2987727a9da8c2057
Kieran Colford <kieran@kcolford.com>
2019-07-25 15:12:03 -0400

    need another beer

--- build-usb.sh
+++ build-usb.sh
@@ -6,6 +6,18 @@ sgdisk -n 0:0:+1M -t 0:ef02 "$1" # grub boot
 sgdisk -n 0:0:0 -t 0:ef00 "$1"	 # data and efi
 sgdisk -h 2 "$1"		 # hybrid main
 sfdisk -Y dos -A "$1" 2		 # bootable main
+sudo partprobe
+
+# setup loopback
+f="$1"
+[ -b "$1" ] || f="$(sudo losetup -f "$f")"
 
 # make filesystem on 2nd partition
-mkfs -t fat /dev/"$(lsblk -no pkname,kname | awk "/[^0-9]2\$/ && \$1 == \"$(lsblk -no kname "$1")\" { print \$2 }")"
+secondpart="$(lsblk -no pkname,kname | awk "/[^0-9]2\$/&&\$1==\"$(lsblk -no kname "$f")\"{print \$2}")"
+sudo mkfs.fat /dev/"$secondpart"
+
+sudo mount "$f" /mnt
+sudo cp -r "$(dirname "$0")"/*.sh /mnt/
+sudo umount "$f"
+
+losetup -d "$f"
