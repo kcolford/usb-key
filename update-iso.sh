bf8b924e7ee1431ab43afac0581d822297eb4773
Kieran Colford <kieran@kcolford.com>
2019-07-25 14:37:11 -0400

    update-iso: cleaned up variable assignments

--- update-iso.sh
+++ update-iso.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 # look at device
 d="$(dirname "$0")"
-read -r fsdev < <(df -P "$d" | awk 'END{print $1}')
-read -r fsuuid < <(lsblk "$fsdev" -no uuid)
-read -r bootdev < <(lsblk "$fsdev" -no pkname)
+fsdev="$(df -P "$d" | awk 'END{print $1}')"
+fsuuid="$(lsblk "$fsdev" -no uuid)"
+bootdev="$(lsblk "$fsdev" -no pkname)"
 
 bsdtar -x -f "$1" -C "$d"
 sed -i "s|archisolabel=\w*|archisodevice=/dev/disk/by-uuid/$fsuuid|g" "$d"/{arch,loader}/**
